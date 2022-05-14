bb3f84bc2dd6531b907d141dafec7b4659735f7c
Grace Thompson <Grace.thompson17@aol.com>
2022-05-14 15:51:15 -0500

    Dried employer

---
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -0,0 +1,50 @@
+package org.launchcode.techjobs.oo;
+
+import java.util.Objects;
+
+public abstract class JobField {
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+    public JobField() {
+        id = nextId;
+        nextId++;
+    }
+
+    public JobField(String value) {
+        this();
+        this.value = value;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object p){
+        if(this==p)return true;
+        if(p==null)return false;
+        if(!(p instanceof JobField))return false;
+        return getId() == ((JobField) p).getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
